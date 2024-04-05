83f696042f37841935a54a928c68d0cf4fd152d2
DiskoSchmitt <162830426+DiskoSchmitt@users.noreply.github.com>
2024-04-05 05:02:29 +0200

    Update buildOrderParams.ts
    Percentage Order Update

--- src/services/apexpro/buildOrderParams.ts
+++ src/services/apexpro/buildOrderParams.ts
@@ -3,104 +3,78 @@ import config = require('config');
 import { AlertObject } from '../../types';
 import 'dotenv/config';
 import { getDecimalPointLength, getStrategiesDB } from '../../helper';
-import {CreateOrderOptionsObject, generateRandomClientId, OrderType} from "apexpro-connector-node";
-import {Market} from "apexpro-connector-node/lib/apexpro/interface";
+import { CreateOrderOptionsObject, generateRandomClientId, OrderType } from "apexpro-connector-node";
+import { Market } from "apexpro-connector-node/lib/apexpro/interface";
 import { BigNumber } from 'bignumber.js';
 
 export const apexproBuildOrderParams = async (alertMessage: AlertObject) => {
-	const [db, rootData] = getStrategiesDB();
+    const [db, rootData] = getStrategiesDB();
 
-	// set expiration datetime. must be more than 1 minute from current datetime
-	const date = new Date();
-	date.setMinutes(date.getMinutes() + 2);
-	const dateStr = date.toJSON();
+    const date = new Date();
+    date.setMinutes(date.getMinutes() + 2);
+    const dateStr = date.toJSON();
 
-	const connector = await ApexproConnector.build();
+    const connector = await ApexproConnector.build();
 
-	let market = alertMessage.market;
-	if (market.endsWith("USD")) {
-		market = market.replace("USD", "USDC");
-	}
+    let market = alertMessage.market;
+    if (market.endsWith("USD")) {
+        market = market.replace("USD", "USDC");
+    }
 
-	const marketsData = await connector.GetSymbolData(market);
+    const marketsData = await connector.GetSymbolData(market);
+    if (!marketsData) {
+        console.log('Market data error, symbol=' + market);
+        throw new Error('Market data error, symbol=' + market);
+    }
+    console.log('Market Data', marketsData);
 
-	if (!marketsData){
-		console.log('markets is error, symbol=' + market);
-		throw new Error('markets is error, symbol=' + market);
-	}
+    const tickerData = await connector.client.publicApi.tickers(marketsData.crossSymbolName);
+    console.log('Ticker Data', tickerData);
+    if (tickerData.length == 0) {
+        console.error('Ticker data is error, symbol=' + marketsData.crossSymbolName);
+        throw new Error('Ticker data error, symbol=' + marketsData.crossSymbolName);
+    }
 
-	console.log('marketsData', marketsData);
+    const orderSide = alertMessage.order === 'buy' ? "BUY" : "SELL";
 
-	const tickerData = await connector.client.publicApi.tickers(marketsData.crossSymbolName);
-	console.log('tickerData', tickerData);
-	if (tickerData.length == 0) {
-		console.error('tickerData is error');
-		throw new Error('tickerData is error, symbol=' + marketsData.crossSymbolName);
-	}
+    const tradeMarginPercentage = new BigNumber(process.env.TRADE_MARGIN_PERCENTAGE || '100').div(100);
+    const poolAvailableAmount = new BigNumber(100); // Assume an example available balance
+    let orderSize = availableBalance.multipliedBy(tradeMarginPercentage);
 
+    const stepSize = new BigNumber(marketsData.stepSize);
+    const orderSizeStr = orderSize.div(stepSize).dp(0, BigNumber.ROUND_DOWN).multipliedBy(stepSize).toFixed();
 
-	const orderSide =
-		alertMessage.order == 'buy' ? "BUY" : "SELL";
+    const latestPrice = new BigNumber(tickerData.at(0).oraclePrice);
+    const tickSize = new BigNumber(marketsData.tickSize);
 
-	let orderSize: number;
-	if (
-		alertMessage.reverse &&
-		rootData[alertMessage.strategy].isFirstOrder == 'false'
-	) {
-		orderSize = alertMessage.size * 2;
-	} else {
-		orderSize = alertMessage.size;
-	}
+    const slippagePercentage = new BigNumber(0.05);
+    const minPrice = orderSide === "BUY"
+        ? latestPrice.multipliedBy(new BigNumber(1).plus(slippagePercentage))
+        : latestPrice.multipliedBy(new BigNumber(1).minus(slippagePercentage));
+    const price = minPrice.minus(minPrice.mod(tickSize)).toFixed();
 
-	const stepSize = parseFloat(marketsData.stepSize);
-	let orderSizeStr;
+    const fee = new BigNumber(config.get('Apexpro.User.limitFee')).multipliedBy(price).multipliedBy(orderSizeStr);
+    console.log('Fee:', fee.toString());
 
-	if(stepSize  >= 1){
-		orderSizeStr =  new BigNumber(orderSize).div(stepSize).dp(0,BigNumber.ROUND_DOWN).multipliedBy(stepSize).toFixed(0,BigNumber.ROUND_DOWN)
-	}else{
-		const stepDecimal = getDecimalPointLength(stepSize);
-		orderSizeStr = Number(orderSize).toFixed(stepDecimal);
-	}
+    const currency_info = connector.symbols.currency.find(item => item.id === marketsData.settleCurrencyId);
+    const limitFee = fee.toFixed(currency_info.starkExResolution.length - 1, BigNumber.ROUND_UP);
+    console.log('Limit Fee:', limitFee.toString());
 
+    const apiOrder: CreateOrderOptionsObject = {
+        limitFee: limitFee.toString(),
+        price: price,
+        reduceOnly: false,
+        side: orderSide,
+        size: orderSizeStr,
+        symbol: <Market>market,
+        timeInForce: 'FILL_OR_KILL',
+        type: OrderType.MARKET,
+        clientOrderId: generateRandomClientId(),
+        positionId: connector.positionID,
+        trailingPercent: '',
+        triggerPrice: '',
+    };
 
-	const latestPrice = parseFloat(tickerData.at(0).oraclePrice);
-	const tickSize = parseFloat(marketsData.tickSize);
-	console.log('latestPrice', latestPrice);
-
-	const slippagePercentage = 0.05;
-	const minPrice =
-		orderSide == "BUY"
-			? latestPrice * (1 + slippagePercentage)
-			: latestPrice * (1 - slippagePercentage);
-
-	const priceBN = new BigNumber(minPrice);
-	const price = priceBN.minus(priceBN.mod(tickSize)).toFixed();
-
-	//const decimal = getDecimalPointLength(tickSize);
-	//const price = minPrice.toFixed(decimal);
-
-	const fee = parseFloat(config.get('Apexpro.User.limitFee')) * parseFloat(price) * parseFloat(orderSizeStr)
-	console.log('fee: ', fee.toString());
-
-	const currency_info: any = connector.symbols.currency.find((item) => item.id == marketsData.settleCurrencyId);
-
-	const limitFee = BigNumber(fee).toFixed(currency_info.starkExResolution.length - 1, BigNumber.ROUND_UP)
-	console.log('limitFee: ', limitFee.toString());
-	const apiOrder: CreateOrderOptionsObject = {
-		limitFee: limitFee.toString(),
-		price: price,
-		reduceOnly: false,
-		side: orderSide,
-		size: orderSizeStr,
-		symbol: <Market>market,
-		timeInForce: 'FILL_OR_KILL',
-		type: OrderType.MARKET,
-		clientOrderId: generateRandomClientId(),
-		positionId: connector.positionID,
-		trailingPercent: '',
-		triggerPrice: '',
-	} ;
-
-	console.log('apiOrder for apexpro', apiOrder);
-	return apiOrder;
+    console.log('API Order for Apex:', apiOrder);
+    return apiOrder;
 };
